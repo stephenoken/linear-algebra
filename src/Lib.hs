b271468d5e91bf16a527e4e67040ecc18a797952
Stephen O'Kennedy <stephen.okennedy@mttnow.com>
2018-01-17 17:43:21 +0000

    Working through Lesson 2   * Created the Vector type   * Implemented addition/subtracting/scalar multiplication operations

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,30 @@
 module Lib
-    ( someFunc,
-      add5
+    (Vector,
+     plus,
+     subtract,
+     scalarM
     ) where
+import Prelude hiding (subtract)
+import Data.Decimal
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Data Types
+data Vector = Vector [Decimal]
+ 
+instance Eq Vector where
+  Vector x == Vector y = x == y
 
-add5 :: (Num a) => a -> a
-add5 = (+) 10
+instance Show Vector where
+  show (Vector x) = "V" ++ show x
+
+instance Monoid Vector where
+  mempty = Vector []
+  mappend = plus
+    
+plus :: Vector -> Vector -> Vector 
+plus (Vector x) (Vector y) = Vector $ zipWith (+) x y 
+
+subtract :: Vector -> Vector -> Vector
+subtract (Vector x) (Vector y) = Vector $ zipWith (-) x y 
+
+scalarM ::Decimal -> Vector -> Vector
+scalarM a (Vector x) = Vector $ map (* a) x 
